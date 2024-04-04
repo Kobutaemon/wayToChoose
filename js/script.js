6cee65ff56b957bd8f34c1f388de6fbe93925d63
Kobutaemon <isotoa1219@gmail.com>
2024-04-04 15:18:14 +0900

    first commit

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function notePc() {
+    window.location.href = "notePc.html";
+};
+
+function earPhone() {
+    window.location.href = "earPhone.html";
+};
+
+function smartPhone() {
+    window.location.href = "smartPhone.html";
+};
